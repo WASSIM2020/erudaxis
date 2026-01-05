00d09ba3713f274bcd9dad7b1de60486f490efc3
Wassim Ghanmi <61029860+WASSIM2020@users.noreply.github.com>
2026-01-05 13:53:38 +0100

    Update Runner.java

--- src/test/java/run/Runner.java
+++ src/test/java/run/Runner.java
@@ -1,25 +1,24 @@
-package Runner;
-
-
-import org.junit.runner.RunWith;
+package run;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-
 @CucumberOptions(
+    features = "features/Ajout_collaborateur.feature",  // parfait, car sous src/test/resources/features
+    // ou features = "features" pour exécuter tous les .feature du dossier
 
-		features = {"features/Ajout_collaborateur.feature"},
+    glue = "StepDef",  // à ajuster si ton package de steps est différent (ex: "stepdef" ou "stepDefinition")
 
-		glue="StepDef",
+    monochrome = true,
 
-		plugin = {"pretty",
-				  "html:target/Cucumber-report.html",
-				  "json:target/cucumber.json",
-				  "junit:target/cucumber-report.xml"
-				 }
-		)
+    plugin = {
+        "pretty",
+        "html:target/cucumber-html-report",
+        "json:target/cucumber.json",         
+        "junit:target/cucumber.xml"
+    }
+)
 public class Runner {
-
 }
